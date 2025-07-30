3dc5f46099e0dc1dc5ea43e1750ae513e88d8982
nazaporta <nazarena@porta.com.py>
2025-07-29 22:52:47 -0300

    remove sendConfirmationEmail to AppointmentList

--- resources/js/components/AppointmentList.vue
+++ resources/js/components/AppointmentList.vue
@@ -183,29 +183,6 @@ watch(
     { deep: true }
 )
 
-const sendConfirmationEmail = (appointment) => {
-    // Simulación de envío de email
-    const { patient, date } = appointment;
-
-    // Convertir la fecha a formato legible
-    const formattedDate = new Date(date).toLocaleDateString('es-AR', {
-        weekday: 'long',
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-    });
-
-    const formattedTime = new Date(date).toLocaleTimeString('es-AR', {
-        hour: '2-digit',
-        minute: '2-digit',
-    });
-
-    alert(`Correo CONFIRMACIÓN de enviado a profesional y cliente para la cita:
-    - Paciente: ${patient}
-    - Fecha: ${formattedDate}
-    - Hora: ${formattedTime}`);
-}
-
 const sendCancellationEmail = (appointment) => {
     // Simulación de envío de email
     const { patient, date } = appointment;
